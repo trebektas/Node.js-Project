eb27e21ef0023b7832e7b7b76451d6ee76fd9634
trebektas <trensarbektas@gmail.com>
2022-11-24 15:54:56 +0100

    run prettier

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -5,53 +5,51 @@ const request = supertest(app);
 
 describe("check statusCode /movie", () => {
   it("statusCode should be 200 for the get request (getMovies)", async () => {
-    const getResponse = await request.get('/movie');
+    const getResponse = await request.get("/movie");
     expect(getResponse.statusCode).toBe(200);
   });
 
-
   it("statusCode should be 200 for the post request (addMovie) with a true structure", async () => {
-    const postResponse = await request.post('/movie').send({
-      title:"The Intouchables",
-      director:"Olivier Nakache",
-      release_date:"2012-03-22"
-  });
+    const postResponse = await request.post("/movie").send({
+      title: "The Intouchables",
+      director: "Olivier Nakache",
+      release_date: "2012-03-22",
+    });
     expect(postResponse.statusCode).toBe(200);
   });
 
-
   it("statusCode should be 400 for the post request (addMovie) with a wrong structure", async () => {
-    const postResponse = await request.post('/movie').send({
-      movie:"The Intouchables",
-      director:"Olivier Nakache",
-      release_date:"2012-03-22"
-  });
+    const postResponse = await request.post("/movie").send({
+      movie: "The Intouchables",
+      director: "Olivier Nakache",
+      release_date: "2012-03-22",
+    });
     expect(postResponse.statusCode).toBe(400);
   });
 });
 
-
 describe("check statusCode /movie/:id", () => {
   it("statusCode should be 200 for the get request (searchMovie) with a true id", async () => {
-    const getResponse = await request.get('/movie/ea1ab288-2365-4381-a9f2-729330d00df2');
+    const getResponse = await request.get(
+      "/movie/ea1ab288-2365-4381-a9f2-729330d00df2"
+    );
     expect(getResponse.statusCode).toBe(200);
   });
 
-
   it("statusCode should be 404 for get request (searchMovie) when movie not found", async () => {
-    const getResponse = await request.get('/movie/12345');
+    const getResponse = await request.get("/movie/12345");
     expect(getResponse.statusCode).toBe(404);
   });
-  
-  
+
   it("statusCode should be 200 for the delete request with a true id", async () => {
-    const deleteResponse = await request.delete('/movie/ea1ab288-2365-4381-a9f2-729330d00df2');
+    const deleteResponse = await request.delete(
+      "/movie/ea1ab288-2365-4381-a9f2-729330d00df2"
+    );
     expect(deleteResponse.statusCode).toBe(200);
   });
-  
-  
+
   it("statusCode should be 404 for the post request with a wrong id", async () => {
-    const deleteResponse = await request.delete('/movie/111111');
+    const deleteResponse = await request.delete("/movie/111111");
     expect(deleteResponse.statusCode).toBe(404);
   });
 });
--- routes.js
+++ routes.js
@@ -1,22 +1,27 @@
-import express from 'express';
-import { getMovies, addMovie, searchMovie, removeMovie } from './controllers.js';
+import express from "express";
+import {
+  getMovies,
+  addMovie,
+  searchMovie,
+  removeMovie,
+} from "./controllers.js";
 
-const app = express()
+const app = express();
 
 //parse JSON using express
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //get the movie list in the form of JSON
-app.get("/movie", getMovies)
+app.get("/movie", getMovies);
 
 //add movie to the list
-app.post("/movie", addMovie)
+app.post("/movie", addMovie);
 
 //search for a movie in the list
 app.get("/movie/:id", searchMovie);
 
 //remove movie from the list
-app.delete("/movie/:id", removeMovie)
+app.delete("/movie/:id", removeMovie);
 
 export default app;
